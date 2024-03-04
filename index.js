35a7dc1a556e65988e7b26fa3fbfd5c2fe60984e
BALAND Jérome <baland.jerome@gmail.com>
2024-03-04 20:24:37 +0100

    input range fonctionnel

--- index.js
+++ index.js
@@ -5,38 +5,36 @@
 
 // 7 - Gérer les 3 boutons pour trier (méthode sort()) les pays
 
-// Architecture de la fonction d'affichage
-// monTableau
-// .filter((country)=> country.nomdupays.includes(inputSearch.value))
-// .sort((a,b)=>{
-//   if(...){
-//     return ...
-//   } else if(...){
-//     return(...)
-//   }
-// })
-// .slice(0, inputValue)
 // .map((country)=>`
 // <div class="card>
 
 // </div>
 // `)
 const inputSearch = document.getElementById("inputSearch");
-const test2 = document.querySelector(".filter-container");
 const result = document.querySelector(".countries-container");
 let countries = [];
+let inputValue = 24;
 
 fetchCountry = async () => {
   await fetch("https://restcountries.com/v3.1/all")
     .then((res) => res.json())
     .then((data) => (countries = data));
-
-  // console.log(countriesPopulation);
   countryDisplay();
 };
 
 countryDisplay = () => {
   result.innerHTML = `<ul>${countries
+    .filter((country) =>
+      country.translations.fra.common.includes(inputSearch.value)
+    )
+    // .sort((a, b) => {
+    //   if () {
+    //     return a - b;
+    //   } else if () {
+    //     return b - a;
+    //   }
+    // })
+    .slice(0, inputValue)
     .map(
       (country) =>
         `
@@ -52,8 +50,12 @@ countryDisplay = () => {
 };
 
 inputSearch.addEventListener("input", (e) => {
-  console.log(e.target.value);
-  countryDisplay(e.target.value);
+  countryDisplay();
+});
+inputRange.addEventListener("input", (e) => {
+  inputValue = inputRange.value;
+  rangeValue.textContent = `${inputValue}`;
+  countryDisplay();
 });
 
 fetchCountry();
