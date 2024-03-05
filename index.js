e553aa126af0df17580079ddc5c0cbb7f1584fbf
BALAND Jérome <baland.jerome@gmail.com>
2024-03-05 19:45:31 +0100

    reste tri alphabétique

--- index.js
+++ index.js
@@ -1,20 +1,10 @@
-// 5 - Récupérer ce qui est tapé dans l'input et filtrer (avant le map) les données
-// coutry.name.includes(inputSearch.value);
-
-// 6 - Avec la méthode Slice gérer le nombre de pays affichés (inputRange.value)
-
 // 7 - Gérer les 3 boutons pour trier (méthode sort()) les pays
 
-// .map((country)=>`
-// <div class="card>
-
-// </div>
-// `)
 const inputSearch = document.getElementById("inputSearch");
 const result = document.querySelector(".countries-container");
 let countries = [];
 let inputValue = 24;
-let min, max, alphabetique;
+let min, max;
 
 fetchCountry = async () => {
   await fetch("https://restcountries.com/v3.1/all")
@@ -33,8 +23,6 @@ countryDisplay = () => {
         return a.population - b.population;
       } else if (max) {
         return b.population - a.population;
-      } else if (alphabetique) {
-        return a.translations.fra.common - b.translations.fra.common;
       }
     })
     .slice(0, inputValue)
@@ -63,19 +51,11 @@ inputRange.addEventListener("input", (e) => {
 minToMax.addEventListener("click", () => {
   min = 1;
   max = null;
-  alphabetique = null;
   countryDisplay();
 });
 maxToMin.addEventListener("click", () => {
   min = null;
   max = 1;
-  alphabetique = null;
-  countryDisplay();
-});
-alpha.addEventListener("click", () => {
-  min = null;
-  max = null;
-  alphabetique = 1;
   countryDisplay();
 });
 
